3d33667c0fe5a14ca647982a0264c6e9ed0f56dd
Alexander Privalov <ckpunmkug@gmail.com>
2024-10-27 19:12:56 +0500

    -

--- map/README.md
+++
@@ -1,5 +0,0 @@
-``` 
-wget https://github.com/ckpunmkug/tools/raw/main/map/webmap.phar
-chmod 755 ./webmap.phar
-./webmap.phar --help
-
--- map/src/include/Settings.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Settings
-{
-	static $ip = '';
-	static $network = '';
-	static $data_dir_name = '';
-	static $country_names_abbr = [];
-	static $state_file_name = "statistics.json";
-}
